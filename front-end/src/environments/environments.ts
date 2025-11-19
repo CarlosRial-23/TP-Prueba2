825f05cbd4c7e5e569e9c466833a17e2355494fa
CarlosRial-23 <rialcarlos2009@hotmail.com>
2025-11-19 19:01:05 -0300

    Inicio Spring 4

--- front-end/src/environments/environments.ts
+++ front-end/src/environments/environments.ts
@@ -2,7 +2,7 @@ export const environment = {
   supabaseUrl: 'https://lxepvjjbxffgpejbribv.supabase.co',
   supabaseKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4ZXB2ampieGZmZ3BlamJyaWJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5ODc4NTUsImV4cCI6MjA3ODU2Mzg1NX0.Bu-z4cFc68KPK1FhjlB54uQqjdFJLbnOdRHr-tAU-EU',
-  //apiUrl: 'https://carlos-rial-tp2-prog4-2025-c2.onrender.com',
+    //apiUrl: 'https://carlos-rial-tp2-prog4-2025-c2.onrender.com',
 
-  apiUrl: 'http://localhost:3000',
+    apiUrl: 'http://localhost:3000',
 };
